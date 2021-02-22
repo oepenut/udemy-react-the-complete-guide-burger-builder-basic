a7815e41c58a3fcb86ce4de62f7cf123e02d7296
Oscar Penut <openut@applaudostudios.com>
2021-02-22 16:35:11 -0600

    :wrench: create constants file

---
+++ src/utils/constanst.ts
@@ -0,0 +1,20 @@
+export enum IngredientsNames {
+  topBread = 'topBread',
+  salad = 'salad',
+  bacon = 'bacon',
+  cheese = 'cheese',
+  meat = 'meat',
+  bottomBread = 'bottomBread',
+}
+
+export enum IngredientsReducerActions {
+  ADD_SALAD = 'ADD_SALAD',
+  REMOVE_SALAD = 'REMOVE_SALAD',
+  ADD_BACON = 'ADD_BACON',
+  REMOVE_BACON = 'REMOVE_BACON',
+  ADD_CHEESE = 'ADD_CHEESE',
+  REMOVE_CHEESE = 'REMOVE_CHEESE',
+  ADD_MEAT = 'ADD_MEAT',
+  REMOVE_MEAT = 'REMOVE_MEAT',
+  CLEAR_INGREDIENTS = 'CLEAR_INGREDIENTS',
+}
