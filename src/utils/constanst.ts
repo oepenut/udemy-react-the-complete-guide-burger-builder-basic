3b23c1fe06917e572c4ae4a5f8bba5219708905c
Oscar Penut <openut@applaudostudios.com>
2021-02-25 18:47:22 -0600

    :clown_face: add ingredient prices enum

--- src/utils/constanst.ts
+++ src/utils/constanst.ts
@@ -7,6 +7,13 @@ export enum IngredientsNames {
   bottomBread = 'bottomBread',
 }
 
+export enum IngredientsPrices {
+  salad = 0.6,
+  bacon = 0.8,
+  cheese = 0.3,
+  meat = 1.3,
+}
+
 export enum IngredientsReducerActions {
   ADD_SALAD = 'ADD_SALAD',
   REMOVE_SALAD = 'REMOVE_SALAD',
