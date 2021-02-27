8dec64370abb274ce0d7a6be3015d59aabe68266
Oscar Penut <openut@applaudostudios.com>
2021-02-26 18:43:47 -0600

    :recycle: update app component classes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Layout from 'components/Layout';
 import BurgerBuilder from 'containers/BurgerBuilder';
-import './App.css';
+import Styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome Burguer Eater</h1>
+    <div className={Styles.App}>
+      <h1 className={Styles.Centered}>Welcome Burguer Eater</h1>
       <Layout>
         <BurgerBuilder />
       </Layout>
