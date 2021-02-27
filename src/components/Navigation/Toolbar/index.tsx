fc08764ec0afbc1ce06ced50210f51d44d496624
Oscar Penut <openut@applaudostudios.com>
2021-02-26 18:42:30 -0600

    :sparkles: create toolbar component

---
+++ src/components/Navigation/Toolbar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Styles from './toolbar.module.css';
+
+const index = () => {
+  return (
+    <header className={Styles.Toolbar}>
+      <div>Menu</div>
+      <div>LOGO</div>
+      <nav>nav...</nav>
+    </header>
+  );
+};
+
+export default index;
