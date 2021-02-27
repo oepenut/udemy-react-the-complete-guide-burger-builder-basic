f7d7c995a16d499b0d3cddc6cad73c2dcd707446
Oscar Penut <openut@applaudostudios.com>
2021-02-26 18:45:08 -0600

    :sparkles: update modal component

--- src/components/UI/Modal/index.tsx
+++ src/components/UI/Modal/index.tsx
@@ -1,13 +1,19 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, Fragment } from 'react';
+import Backdrop from 'components/UI/Backdrop';
 import Styles from './modal.module.css';
 
 type Props = {
   children?: ReactNode;
+  show: boolean;
+  hideModal: () => void;
 };
 
-const index = (props: Props) => {
-  const { children } = props;
-  return <div className={Styles.Modal}>{children}</div>;
-};
+const index = (props: Props) =>
+  props.show ? (
+    <Fragment>
+      <Backdrop show={props.show} hideModal={props.hideModal} />
+      <div className={Styles.Modal}>{props.children}</div>
+    </Fragment>
+  ) : null;
 
 export default index;
