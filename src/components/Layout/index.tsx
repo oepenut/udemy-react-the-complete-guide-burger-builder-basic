5b53c45c81c08bc0bfd0e723b196d83902c6eeb1
Oscar Penut <openut@applaudostudios.com>
2021-03-01 19:34:17 -0600

    :sparkles: update layout component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,19 +1,28 @@
-import React, { ReactNode, Fragment } from 'react';
+import React, { ReactNode, Fragment, useState } from 'react';
+import SideDrawer from 'components/Navigation/SideDrawer';
 import Toolbar from 'components/Navigation/Toolbar';
 
 type Props = {
   children: ReactNode;
 };
 
-const index = (props: Props) => {
+const Index = (props: Props) => {
   const { children } = props;
+  const [sideDrawerShow, setSideDrawerShow] = useState(true);
+
+  const hideSideDrawer = () => setSideDrawerShow(false);
+  const showSideDrawer = () => setSideDrawerShow(true);
 
   return (
     <Fragment>
-      <Toolbar />
+      <SideDrawer
+        showSideDrawer={sideDrawerShow}
+        hideSideDrawer={hideSideDrawer}
+      />
+      <Toolbar showSideDrawer={showSideDrawer} />
       <div>{children}</div>
     </Fragment>
   );
 };
 
-export default index;
+export default Index;
