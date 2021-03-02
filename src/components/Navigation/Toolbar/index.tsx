e91637f6563207f2435cb129e5c7358e6d63c59a
Oscar Penut <openut@applaudostudios.com>
2021-03-01 19:33:44 -0600

    :sparkles: update toolbar component

--- src/components/Navigation/Toolbar/index.tsx
+++ src/components/Navigation/Toolbar/index.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
+import Logo from 'components/UI/Logo';
+import Navbar from '../Navbar';
 import Styles from './toolbar.module.css';
 
-const index = () => {
+const navItems = {
+  items: [
+    {
+      src: '/',
+      label: 'Home',
+    },
+    {
+      src: '/payment',
+      label: 'Payment',
+    },
+  ],
+  activeUrl: '/',
+};
+
+type Props = {
+  showSideDrawer: () => void;
+};
+
+const index = (props: Props) => {
+  const { showSideDrawer } = props;
+
   return (
     <header className={Styles.Toolbar}>
-      <div>Menu</div>
-      <div>LOGO</div>
-      <nav>nav...</nav>
+      <div onClick={showSideDrawer}>Menu</div>
+      <div className={Styles.Logo}>
+        <Logo />
+      </div>
+      <nav className={Styles.DesktopOnly}>
+        <Navbar navItems={navItems} />
+      </nav>
     </header>
   );
 };
